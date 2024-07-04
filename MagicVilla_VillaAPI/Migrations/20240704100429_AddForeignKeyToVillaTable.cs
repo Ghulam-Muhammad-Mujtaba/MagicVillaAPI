b27ce1a6cc2c38fde3ea95c34aa4939298c7064c
Ghulam-Muhammad-Mujtaba <gmmujtaba21@gmail.com>
2024-07-04 15:33:43 +0500

    Add project files.

---
+++ MagicVilla_VillaAPI/Migrations/20240704100429_AddForeignKeyToVillaTable.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVillaVillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyToVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VillaNumbers",
+                table: "VillaNumbers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "VillaID",
+                table: "VillaNumbers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VillaNumbers",
+                table: "VillaNumbers",
+                column: "VillaID");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 15, 4, 29, 580, DateTimeKind.Local).AddTicks(6276));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 15, 4, 29, 580, DateTimeKind.Local).AddTicks(6289));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 15, 4, 29, 580, DateTimeKind.Local).AddTicks(6291));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 15, 4, 29, 580, DateTimeKind.Local).AddTicks(6293));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 15, 4, 29, 580, DateTimeKind.Local).AddTicks(6295));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VillaNumbers_Villas_VillaID",
+                table: "VillaNumbers",
+                column: "VillaID",
+                principalTable: "Villas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VillaNumbers_Villas_VillaID",
+                table: "VillaNumbers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VillaNumbers",
+                table: "VillaNumbers");
+
+            migrationBuilder.DropColumn(
+                name: "VillaID",
+                table: "VillaNumbers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VillaNumbers",
+                table: "VillaNumbers",
+                column: "VillaNo");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 14, 11, 27, 200, DateTimeKind.Local).AddTicks(7538));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 14, 11, 27, 200, DateTimeKind.Local).AddTicks(7552));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 14, 11, 27, 200, DateTimeKind.Local).AddTicks(7554));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 14, 11, 27, 200, DateTimeKind.Local).AddTicks(7560));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 4, 14, 11, 27, 200, DateTimeKind.Local).AddTicks(7562));
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/VillaNumber.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models
+{
+    public class VillaNumber
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)] //Unique Key but not identity
+
+        [ForeignKey("Villa")]
+        public int VillaID { get; set; }
+        public Villa Villa { get; set; }
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
